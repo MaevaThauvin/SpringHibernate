f964beb08b4898ed7575045845e9ab2f465a2703
Maëva Thauvin <maeva.thauvin@cimpa.com>
2021-02-15 17:10:50 +0100

    update with movie repo tests findBy method

--- src/test/java/movieapp/persistence/mongodb/MovieRepositoryTest.java
+++ src/test/java/movieapp/persistence/mongodb/MovieRepositoryTest.java
@@ -2,9 +2,19 @@ package movieapp.persistence.mongodb;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import movieapp.persistence.document.Artist;
 import movieapp.persistence.document.Movie;
@@ -14,6 +24,9 @@ class MovieRepositoryTest {
 	
 	@Autowired
 	MovieRepository movieRepository;
+	
+	int i =0;
+	static int year=1977;
 
 	@Test
 	void testCreateBuilder() {
@@ -23,10 +36,66 @@ class MovieRepositoryTest {
 				.year(2006)
 				.director(director)
 				.build();
-		System.out.println(movie);
+		// System.out.println(movie);
 		// save in repo mongodb
 		movieRepository.save(movie);
+	}
+	
+	@Test
+	void testFindAll() {
 		movieRepository.findAll().forEach(System.out::println);
 	}
-
+	
+	@ParameterizedTest
+	@ValueSource(ints={0,1, 19})
+	void testFindAllPagination(int numPage) {
+		int size= 5;
+		var movie = movieRepository.findAll(PageRequest.of(numPage, size))
+				.map(Movie::getTitle)
+				.toList();
+		System.out.println(movie);
+	}
+	
+	static IntStream numberMovie(){
+		// return Stream.of(1, 2, 3, 4, 5, 6, 7);
+		return IntStream.rangeClosed(1, 100);
+		
+	}
+	
+	@Test //After each
+	void testDeleteAll() {
+		movieRepository.deleteAll();
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("numberMovie")
+	void testCeateFranchise(int number) {
+		var movie = Movie.of("Star Wars "+ number, year);
+		year++;
+		movieRepository.save(movie);
+		System.out.println("After saving: "+movie);
+	}
+	
+	@Test
+	void testFindByDirectorName() {
+		String name = "Martin Campbell";
+		var movies = movieRepository.findByDirectorName(name)
+				.collect(Collectors.toList());
+		System.out.println(movies);
+		
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints= {0,1,19})
+	void TestFindByYearGreaterThanOrderByYear(int numPage) {
+		int yearMin = 2000;
+		int size = 5;
+		var movies = movieRepository.findByYearGreaterThanOrderByYear(yearMin, PageRequest.of(numPage, size))
+				.map(Movie::getYear)
+				.toList();
+		System.out.println(movies);
+				
+		
+	}
 }
